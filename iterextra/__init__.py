c065a1af4727d5570d36fe9763a966157bab8cd6
Adam Kaliński <adam.kalinski@grupaonet.pl>
2013-09-11 12:19:45 +0200

    added docstring

--- iterextra/__init__.py
+++ iterextra/__init__.py
@@ -13,4 +13,14 @@ def _chain_calls(callables, *args, **kwargs):
 
 
 def chaincall(*args):
+    """Chains function calls so that result of previous function is passed as
+    argument to next one.
+
+    >>> to_bool = chaincall(int, bool)
+    >>> to_bool("1")
+    True
+    >>> to_bool("0")
+    False
+    """
+
     return partial(_chain_calls, args)
