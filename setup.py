e7f72c00e24b29b9ddc783ac49a89aee671f645f
Adam Kaliński <adam.kalinski@grupaonet.pl>
2013-09-11 12:09:03 +0200

    inc version ;)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ config = {
     'author': 'Adam Kaliński',
     'url': 'https://github.com/code22/iterextra',
     'download_url': 'https://github.com/code22/iterextra/archive/master.zip',
-    'version': '0.1',
+    'version': '0.1.1',
     'install_requires': ['nose'],
     'packages': ['iterextra'],
     'scripts': [],
