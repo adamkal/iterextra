8a6e4aeee621a1ae6045efe68d2e32a02f489bbd
Adam Kaliński <adam.kalinski@grupaonet.pl>
2013-09-11 11:31:41 +0200

    pyscaffold project structure

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'An extension to Python standard library itertools',
+    'author': 'Adam Kaliński',
+    'url': 'https://github.com/code22/iterextra',
+    'download_url': 'https://github.com/code22/iterextra/archive/master.zip',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['iterextra'],
+    'scripts': [],
+    'name': 'iterextra'
+}
+
+setup(**config)
